e10020fb4b202dd08de4f95e7d807879eccd37ec
sanix <sanicksikani@gmail.com>
2021-04-07 18:56:32 +0300

    POST GET with gorilla/mux

--- main.go
+++ main.go
@@ -2,29 +2,76 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"time"
 
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type DB struct {
+	collection *mongo.Collection
+}
+
 type Movie struct {
-	Name      string   `bson:"name"`
-	Year      string   `bson:"year"`
-	Directors []string `bson:"directors"`
-	Writers   []string `bson:"writers"`
-	BoxOffice `bson:"boxOffice"`
+	ID        interface{} `json:"id" bson:"_id, omitempty"`
+	Name      string      `json:"name" bson:"name"`
+	Year      string      `json:"year" bson:"year"`
+	Directors []string    `json:"directors" bson:"directors"`
+	Writers   []string    `json:"writers" bson:"writers"`
+	BoxOffice BoxOffice   `json:"boxOffice" bson:"boxOffice"`
 }
 
 type BoxOffice struct {
-	Budget uint64 `bson:"budget"`
-	Gross  uint64 `bson:"gross"`
+	Budget uint64 `json:"budget" bson:"budget"`
+	Gross  uint64 `json:"gross" bson:"gross"`
 }
 
 //the mongo-driver package uses another package
 //called bson to serialize Go structs into BSON format.
+
+//GetMovie fetches a movie with a given ID
+func (db *DB) GetMovie(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var movie Movie
+	objectID, _ := primitive.ObjectIDFromHex(vars["id"])
+	filter := bson.M{"_id": objectID}
+	err := db.collection.FindOne(context.TODO(), filter).Decode(&movie)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	} else {
+		w.Header().Set("content-Type", "application/json")
+		response, _ := json.Marshal(movie)
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
+	}
+
+}
+
+func (db *DB) PostMovie(w http.ResponseWriter, r *http.Request) {
+	var movie Movie
+	postBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(postBody, &movie)
+	result, err := db.collection.InsertOne(context.TODO(), movie)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		response, _ := json.Marshal(result)
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
+	}
+}
 
 func main() {
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
@@ -33,45 +80,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer client.Disconnect(context.TODO())
 
-	err = client.Ping(context.TODO(), nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
 	fmt.Println("Connected to MongoDB successfully ")
 
 	collection := client.Database("appDB").Collection("movies")
+	db := &DB{collection: collection}
 
-	//Create  a Movie
-	darkNight := Movie{
-		Name:      "The Dark Knight",
-		Year:      "2008",
-		Directors: []string{"Christopher Nolan"},
-		Writers:   []string{"Jonathan Nolan", "Christopher Nolan"},
-		BoxOffice: BoxOffice{
-			Budget: 185000000,
-			Gross:  533331538,
-		},
+	//http routes
+	r := mux.NewRouter()
+	r.HandleFunc("/v1/movies/{id:[a-zA-Z0-9]*}", db.GetMovie).Methods("GET")
+	r.HandleFunc("/v1/movies", db.PostMovie).Methods("POST")
+
+	//start server
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
-
-	//Insert a document into MongoDB
-	_, err = collection.InsertOne(context.TODO(), darkNight)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//Retrieve
-	queryResult := &Movie{}
-	//bson.M is used for building map for filter query
-	filter := bson.M{"boxOffice.budget": bson.M{"$gt": 15000000}} //$gt => greater than
-	result := collection.FindOne(context.TODO(), filter)
-	err = result.Decode(queryResult)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Movie:", queryResult)
+	log.Fatal(srv.ListenAndServe())
 }
